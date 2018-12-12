50d7d3ad4b28ee19a2308f3da2d9fa6467654ded
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 09:53:34 +0100

    Exercise 4

---
+++ src/main/java/Lexicon_Java_victor/Exercise_4/App.java
@@ -0,0 +1,22 @@
+package Lexicon_Java_victor.Exercise_4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public static void main( String[] args )
+	    {
+	    	
+	    	
+	    	int a = 23;
+	    	int b = 11;
+	    	int c = 77;
+	    	int sum = a + b + c;
+	    	
+	    	System.out.println(sum / 3);
+	    	
+	    	
+	    }
+}
